76852922d19c3427a8f520cfd946c01b278b924a
Traced <2020800023@qq.com>
2021-08-13 14:28:17 +0800

    增加了互斥锁超轻量级实现

---
+++ library/utils/lock.go
@@ -0,0 +1,20 @@
+package utils
+
+type MutexLock struct {
+	mutex chan struct{}
+}
+
+func (l *MutexLock) Lock() {
+	if l.mutex == nil {
+		l.mutex = make(chan struct{}, 1)
+		// l.Unlock()
+	}
+	l.mutex <- struct{}{}
+}
+
+func (l *MutexLock) Unlock() {
+	if l.mutex == nil {
+		return
+	}
+	<-l.mutex
+}
